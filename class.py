b25c90156db8a7236212d416ce189ce2a2f42e3e
Michal Dyzma <mdyzma@gmail.com>
2015-02-05 00:34:08 +0100

    Moving temp files to .tmp/, par files to .par/. PDB names added

--- class.py
+++
@@ -1,9 +0,0 @@
-class atoms:
-
-    def __init__(self,atom_data[]):
-        residue_name=[]
-        atom_data=[]
-        epsi_data=[]
-        lines_epsi=[]
-        lines_mass=[]
-        lines_pdb=[]
